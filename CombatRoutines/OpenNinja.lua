e9ffb1579a35898b7f3a44d78b24cca619aa30b1
beemerwt <beemerwt@gmail.com>
2022-09-07 09:40:55 -0500

    Implemented AwaitDo to fix mudra

--- CombatRoutines/OpenNinja.lua
+++ CombatRoutines/OpenNinja.lua
@@ -16,8 +16,12 @@ local profile = {
   SkillEnabled   = {}
 }
 
+local AwaitDo = ml_global_information.AwaitDo
+
 local AOE = false
-local currentMudra = nil
+local PerformingMudra = false
+
+local activeTrickAttack = nil
 local lastFrameActive = false
 
 local Buffs = {
@@ -27,7 +31,9 @@ local Buffs = {
   RaijuReady  = 0,
   Doton       = 501,
   Kassatsu    = 497,
-  Suiton      = 507 
+  Suiton      = 507,
+  TenChiJin   = 1186,
+  KamaitachiReady = 2723
 }
 
 local PvPSkills = {
@@ -37,6 +43,7 @@ local PvPSkills = {
   ForkedRaiju   = 29510,
 }
 
+-- TODO: Use non-GCD skills in same loop
 local Skills = {
   ShadeShift      = 2241,
   Hide            = 2245,
@@ -50,10 +57,11 @@ local Skills = {
   Chi             = 2261,
   Jin             = 2263,
   Shukuchi        = 2262,
-  Kassatsu        = 2264,
+  Kassatsu        = 2264, -- No GCD
   ArmorCrush      = 3563,
   TenChiJin       = 7403,
   SecondWind      = 7541,
+  Bloodbath       = 7542, -- No GCD
 
   -- Mudras
   Fuma   = 2265,
@@ -69,8 +77,9 @@ local Skills = {
   ForkedRaiju     = 25777,
   FleetingRaiju   = 25778,
   HakkeMujinsatsu = 16488,
+  Kamaitachi      = 16493,
 
-  DreamWithinADream = 3566,
+  DreamWithinADream = 3566, -- No GCD
     
   -- Basic Combo
   SpinningEdge    = 2240,
@@ -78,10 +87,10 @@ local Skills = {
   AeolianEdge     = 2255,
 
   -- Ninki
-  Meisui          = 1648,
-  Bunshin         = 1649,
-  Bhavacakra      = 7402,
-  Hellfrog        = 7401
+  Meisui          = 16489,
+  Bunshin         = 16493,
+  Bhavacakra      = 7402, -- No GCD
+  Hellfrog        = 7401  -- NO GCD
 }
 
 -- If it's not enabled it's not a part of the IcyVeins rotation...
@@ -98,6 +107,11 @@ local Mudras = {
   Suiton = { Skills.Ten, Skills.Chi, Skills.Jin },
 }
 
+-- TODO: Handle Mug the same
+local function TrickAttackIsActive()
+  return activeTrickAttack ~= nil
+end
+
 local function IsNinjutsuReady()
   local offCd = not ActionList:Get(1, Skills.Ten).isoncd
     and not ActionList:Get(1, Skills.Chi).isoncd
@@ -105,10 +119,32 @@ local function IsNinjutsuReady()
   return PlayerHasBuff(Buffs.Kassatsu) or offCd
 end
 
-local function SetMudra(mudra)
-  if currentMudra ~= nil then return false end
-  currentMudra = table.deepcopy(mudra, true)
-  lastFrameActive = false
+local function ComboMudra(mudra)
+  PerformingMudra = true;
+
+  local first = ActionList:Get(1, mudra[1]);
+  local second = ActionList:Get(1, mudra[2]);
+  local third = nil
+  
+  if mudra[3] ~= nil then
+    third = ActionList:Get(1, mudra[3]);
+  end
+
+  AwaitDo(0, 500, first:Cast(), nil, function()
+    AwaitDo(0, 500, second:Cast(), nil, function()
+      if third == nil then
+        PerformingMudra = false
+        d("Casted Mudra: " .. first.name .. ', ' .. second.name)
+        return
+      end
+
+      AwaitDo(0, 500, third:Cast(), nil, function()
+        d("Casted Mudra: " .. first.name .. ', ' .. second.name .. ', ' .. third.name)
+        PerformingMudra = false
+      end)
+    end)
+  end)
+
   return true
 end
 
@@ -131,52 +167,49 @@ local function Opener()
 end
 
 local function Ninjutsu(numNearby)
-  local useKassatsu = profile.SkillEnabled.Kassatsu
-  local hasKassatsu = PlayerHasBuff(Buffs.Kassatsu)
-
-  if not IsNinjutsuReady() and useKassatsu then
+  if profile.SkillEnabled.Kassatsu and not IsNinjutsuReady() then
     local Kassatsu = ActionList:Get(1, Skills.Kassatsu);
     if Kassatsu:IsReady() then return Kassatsu:Cast() end
   end
 
-  if hasKassatsu or IsNinjutsuReady() then
+  if IsNinjutsuReady() then
     if numNearby > 2 then
       if profile.SkillEnabled.Doton then
         if not PlayerHasBuff(Buffs.Doton) then
-          return SetMudra(Mudras.Doton)
+          return ComboMudra(Mudras.Doton)
         end
       end
 
       if PlayerHasBuff(Buffs.Kassatsu)
         and profile.SkillEnabled.Goka then
-        return SetMudra(Mudras.Goka);
+        return ComboMudra(Mudras.Goka);
       end
       
-      return SetMudra(Mudras.Katon);
+      return ComboMudra(Mudras.Katon);
     else
       -- Use Suiton to set up Trick Attack when there is less than 20 seconds left on Trick Attack's cooldown.
-      if profile.SkillEnabled.Suiton then
+      if profile.SkillEnabled.Suiton and not PlayerHasBuff(Buffs.Suiton) then
         local TrickAttack = ActionList:Get(1, Skills.TrickAttack)
-        if TrickAttack.cdmax - TrickAttack.cd < 20 and not PlayerHasBuff(Buffs.Suiton) then
-          return SetMudra(Mudras.Suiton)
+        if TrickAttack.cdmax - TrickAttack.cd < 20 then
+          return ComboMudra(Mudras.Suiton)
         end
       end
 
       if profile.SkillEnabled.Doton then
         if numNearby > 1 and not PlayerHasBuff(Buffs.Doton) then
-          return SetMudra(Mudras.Doton)
+          return ComboMudra(Mudras.Doton)
         end
       end
 
       local hasKassatsu = PlayerHasBuff(Buffs.Kassatsu)
-      if numNearby > 1 and profile.SkillEnabled.Goka and hasKassatsu then
-        return SetMudra(Mudras.Goka) -- Goka Mekkyaku for multi-target
-      elseif profile.SkillEnabled.Goka and hasKassatsu then
-        return SetMudra(Mudras.Hyosho) -- Hyosho Ranryu for single-target
+      if numNearby > 1 and profile.SkillEnabled.Goka and hasKassatsu and IsCapable(Skills.Goka) then
+        return ComboMudra(Mudras.Goka) -- Goka Mekkyaku for multi-target
+      elseif profile.SkillEnabled.Hyosho and hasKassatsu and IsCapable(Skills.Hyosho) then
+        return ComboMudra(Mudras.Hyosho) -- Hyosho Ranryu for single-target
       end
 
       if profile.SkillEnabled.Raiton then
-        return SetMudra(Mudras.Raiton)
+        return ComboMudra(Mudras.Raiton)
       end
     end
   end
@@ -184,53 +217,31 @@ local function Ninjutsu(numNearby)
   return false
 end
 
-local function CastIfReady(skillId, targetId)
-  local action = ActionList:Get(1, skillId);
-  if table.valid(action) then
-    if targetId ~= nil then
-      if action:IsReady(targetId) then
-        if action:Cast(targetId) then
-          return true
-        end
-      end
-    else
-      if action:IsReady() then
-        if action:Cast() then
-          return true
-        end
-      end
-    end
-  end
-
-  return false
-end
-
-local function BasicCombo(numNearby, target)
+local function BasicCombo(numNearby)
   if not profile.ComboEnabled then return false end
 
   if numNearby > 2 then    
     if Player.lastcomboid == Skills.DeathBlossom then
-      return CastIfReady(Skills.HakkeMujinsatsu)
+      return CastOnSelfIfPossible(Skills.HakkeMujinsatsu)
     else
-      return CastIfReady(Skills.DeathBlossom)
+      return CastOnSelfIfPossible(Skills.DeathBlossom)
     end
   else
-    if Player.lastcomboid == Skills.GustSlash then
-      -- If you have more than 30 seconds left on your Huton buff, use Aeolian Edge as your combo ender instead.
-      if Player.gauge[2] > 30000 then
-        return CastIfReady(Skills.AeolianEdge, target.id)
-      end
+    -- If you have more than 30 seconds left on your Huton buff
+    --  use Aeolian Edge as your combo ender instead.
+    if Player.lastcomboid == Skills.GustSlash and Player.gauge[2] > 30000 then
+      return CastOnTargetIfPossible(Skills.AeolianEdge)
     elseif Player.lastcomboid == Skills.SpinningEdge then
-      return CastIfReady(Skills.GustSlash, target.id)
+      return CastOnTargetIfPossible(Skills.GustSlash)
     else
-      return CastIfReady(Skills.SpinningEdge, target.id)
+      return CastOnTargetIfPossible(Skills.SpinningEdge)
     end
   end
 
   return false
 end
 
-function UseRaiju(numNearby, target)
+function UseRaiju(numNearby)
   if not profile.SkillEnabled.FleetingRaiju and not profile.SkillEnabled.ForkedRaiju then
     return false
   end
@@ -240,77 +251,39 @@ function UseRaiju(numNearby, target)
     return false
   end
 
-  local FleetingRaiju = ActionList:Get(1, Skills.FleetingRaiju);
-  local ForkedRaiju = ActionList:Get(1, Skills.ForkedRaiju);
-
-  -- Should automatically calculate distance, so if not fleeting (standing)
-  --  then we cast Forked (teleports to target)
-  if FleetingRaiju:IsReady(target.id) then
-    return FleetingRaiju:Cast(target.id)
-  elseif ForkedRaiju:IsReady(target.id) then
-    return ForkedRaiju:Cast(target.id)
-  end
-
-  return false
+  return CastOnTargetIfPossible(Skills.FleetingRaiju)
+    or CastOnTargetIfPossible(Skills.ForkedRaiju);
 end
 
-local function MaintainHuton(target)
+local function MaintainHuton()
   -- If player doesn't have Huton, must cast.
   local hutonTimeleft = Player.gauge[2]
   local hasHuton = hutonTimeleft > 0
-  if not hasHuton then
-    return SetMudra(Mudras.Huton)
+  if not hasHuton and IsNinjutsuReady() then
+    return ComboMudra(Mudras.Huton)
   end
 
   if hutonTimeleft < 30000 and profile.SkillEnabled.ArmorCrush then
-    local action = ActionList:Get(1, Skills.ArmorCrush);
     local isImmediate = Player.gauge[2] <= 3000
-    if action:IsReady(target.id) then
-      if isImmediate or Player.lastcomboid == Skills.GustSlash then
-        if action:Cast(target.id) then
-          return true
-        else
-          d("Failed to cast ArmorCrush")
-        end
-      end
+    if isImmediate or Player.lastcomboid == Skills.GustSlash then
+      if CastOnTargetIfPossible(Skills.ArmorCrush) then return true end
     end
   end
 
   return false
 end
 
-local function HandleMudra(target)
-  local isFinished = currentMudra[1] == nil
-
-  if isFinished then
-    local jutsu = ActionList:Get(1, Skills.Ninjutsu);
-    local wasCast = jutsu:Cast(target.id) or jutsu:Cast(Player.id)
-    if wasCast then currentMudra = nil end
-    return wasCast
-  else
-    local action = ActionList:Get(1, currentMudra[1]);
-    if action:Cast() then
-      table.remove(currentMudra, 1)
-      return true
-    end
-  end
-
-  return false
-end
-
-local function ManageNinki(numNearby, target)
+local function ManageNinki(numNearby)
   if not profile.NinkiEnabled then return false end
-
   local ninkiPower = Player.gauge[1]
+  
+  if CastOnTargetIfPossible(Skills.Bunshin) then
+    return true
+  end
 
   -- Use Bunshin as soon as it is off cooldown.
-  local Bunshin = ActionList:Get(1, Skills.Bunshin);
-  if Bunshin:IsReady(Player.id) then
-    if Bunshin:Cast(Player.id) then
-      return true
-    else
-      d("Failed to cast Bunshin")
-    end
+  if CastOnSelfIfPossible(Skills.Bunshin) then
+    return true
   end
 
   -- Ninki Management
@@ -318,97 +291,122 @@ local function ManageNinki(numNearby, target)
   if numNearby <= 2 then
     -- If Trick Attack is up, use all your Ninki on Bhavacakra.
     -- If you are about to overcap Ninki and Trick Attack is not up, use Bhavacakra once.
-    local Bhavacakra = ActionList:Get(1, Skills.Bhavacakra)
-    if Bhavacakra:IsReady(target.id) then
-      local TrickAttackDebuff = GetTargetDebuff(target, Buffs.TrickAttack)
-      if TrickAttackDebuff ~= nil then
-        if Bhavacakra:Cast(target.id) then return true end
-      elseif ninkiPower >= 90 then
-        if Bhavacakra:Cast(target.id) then return true end
+    if TrickAttackIsActive() or ninkiPower >= 85 then
+      if CastOnTargetIfPossible(Skills.Bhavacakra) then
+        return true
       end
     end
   else
     -- Our ninki will now be used for AoE as well.
     -- This means using Hellfrog Medium instead of Bhavacakra
-    local Hellfrog = ActionList:Get(1, Skills.Hellfrog)
-    if Hellfrog:IsReady() then
-      if Hellfrog:Cast() then
-        return true
-      else
-        d("Failed to cast hellfrog")
-      end
+    if CastOnTargetIfPossible(Skills.Hellfrog) then
+      return true
     end
   end
 
   return false
 end
 
+local function TCJ()
+  if not profile.SkillEnabled.TenChiJin then return false end
+  if not TrickAttackIsActive() then return false end
+
+  -- We only ever want to do this if Meisui is up
+  if IsOnCooldown(Skills.Meisui) then return false end
+
+  return CastOnSelfIfPossible(Skills.TenChiJin)
+end
+
+local lasttime = 0
+
 -- The Cast() function is where the magic happens.
 -- Action code should be called and fired here.
 function profile.Cast()
-  if Player == nil then return false end
+  -- Runs on a "separate" thread, so we "lock" until it's done
+  if PerformingMudra then return false end
 
-  local target = Player:GetTarget()
-  if target == nil then return false end
+  -- Update 20x a second?
+  if TimeSince(lasttime) < 50 then return false end
+  lastTime = Now()
+
+  if Player == nil then return false end
+  if not ActionList:IsReady() then return false end
   
   -- ensures we're getting newest state of any actions
   ClearCache()
 
-  -- Don't do anything except activate the mudra combo when mudra is active...
-  if currentMudra ~= nil then
-    -- stuckproofing
-    local isMudraActive = PlayerHasBuff(Buffs.Mudra);
-    if lastFrameActive and not isMudraActive then
-      currentMudra = nil
-      return false
-    end
-    
-    lastFrameActive = isMudraActive
-    return HandleMudra(target)
-  end
+  -- TODO: Add Feint
+  -- TODO: Add Shukuchi
 
-  local attackables = MEntityList("alive,attackable");
-  if not table.valid(attackables) then
+  local target = GetACRTarget()
+  if target == nil then return false end
+  if not target.attackable then return false end
+
+  activeTrickAttack = GetTargetDebuff(Buffs.TrickAttack)
+
+  local playerHasTCJ        = PlayerHasBuff(Buffs.TenChiJin)
+  local playerHasMudra      = PlayerHasBuff(Buffs.Mudra)
+  local playerHasKamaitachi = PlayerHasBuff(Buffs.KamaitachiReady)
+
+  if playerHasTCJ then
+    if CastOnTarget(Skills.Ten) then return true end
+    if CastOnTarget(Skills.Chi) then return true end
+    if CastOnTarget(Skills.Jin) then return true end
     return false
   end
 
-  -- Gets targets within range of AOE attacks centered on player
-  local nearby = FilterByProximity(attackables, Player.pos, 10);
-  if not table.valid(nearby) then
-    nearby = {}
+  -- FORCE CAST FOR MUDRA BEFORE ANYTHING
+  if playerHasMudra then
+    if CastOnSelf(Skills.Ninjutsu) then return true end
+    if CastOnTarget(Skills.Ninjutsu) then return true end
+    return false
+  end
+  
+  if playerHasKamaitachi then
+    if CastOnTarget(Skills.Kamaitachi) then return true end
+    if CastOnTarget(Skills.Bunshin) then return true end
+    return false
   end
 
-  if MaintainHuton(target) then return true end
-  if UseRaiju(#nearby, target) then return true end
-  if ManageNinki(#nearby, target) then return true end
-  if Ninjutsu(#nearby) then return true end
+  local nearby = GetNearbyEnemies(10)
 
+  if #nearby > 0 then
+    d('Num Nearby: ' .. tostring(#nearby))
+  end
+
+  if MaintainHuton() then return true end
+
+  if Player.hp.percent < 35 then
+    if CastOnSelfIfPossible(Skills.Bloodbath) then return true end
+  end
+
+  if UseRaiju(#nearby) then return true end
+  if ManageNinki(#nearby) then return true end
+  if TCJ() then return true end
+
+  -- TA becomes priority when Suiton is active
   if profile.SkillEnabled.TrickAttack then
-    if SkillIsActuallyReadyOnTarget(Skills.TrickAttack, target) then
-      if CastOnTarget(Skills.TrickAttack, target) then return true end
-    end
+    if CastOnTargetIfPossible(Skills.TrickAttack) then return true end
   end
 
-  if profile.SkillEnabled.Assassinate then
-    -- Only use DWD/Assassinate when TrickAttack is up
-    local TrickAttackDebuff = GetTargetDebuff(target, Buffs.TrickAttack)
-    if TrickAttackDebuff ~= nil and #nearby < 3 then
-      local assassinate = ActionList:Get(1, Skills.Assassinate)
-      local dwd = ActionList:Get(1, Skills.DreamWithinADream)
-      if not assassinate.isoncd and not dwd.isoncd then
-        -- Will cast both
-        if assassinate:Cast(target.id) then
-          return true
-        end
+  if profile.SkillEnabled.Meisui then
+    if PlayerHasBuff(Buffs.Suiton) and IsOnCooldown(Skills.TrickAttack) then
+      if CastOnSelfIfPossible(Skills.Meisui) then
+        return true
       end
     end
   end
 
-  if BasicCombo(#nearby, target) then return true end
+  if Ninjutsu(#nearby) then return true end
 
-  if SkillIsActuallyReadyOnTarget(Skills.ThrowingDagger, target) then
-    if CastOnTarget(Skills.ThrowingDagger, target) then return true end
+  if profile.SkillEnabled.Assassinate then
+    if TrickAttackDebuff ~= nil and #nearby < 3 then
+      if CastOnTargetIfPossible(Skills.Assassinate) then return true end
+    end    
   end
+
+  if BasicCombo(#nearby) then return true end
+  if CastOnTargetIfPossible(Skills.ThrowingDagger) then return true end
 
   return false
 end
@@ -428,7 +426,7 @@ function profile.Draw()
   profile.NinkiEnabled   = GUI:Checkbox("Ninki Enabled", profile.NinkiEnabled)
 
   for skill,_ in pairs(Skills) do
-    profile.SkillEnabled[skill] = GUI:Checkbox(k, profile.SkillEnabled[skill])
+    profile.SkillEnabled[skill] = GUI:Checkbox(skill, profile.SkillEnabled[skill])
   end
 
   GUI:End()
@@ -473,7 +471,12 @@ end
 
 -- The OnUpdate() function is fired on the gameloop, like any other OnUpdate function found in FFXIVMinion code.
 function profile.OnUpdate(event, tickcount)
-
+  local mudra = GetPlayerBuff(Buffs.Mudra)
+  if mudra ~= nil then
+    if mudra.stacks ~= lastMudraStacks then
+      wasMudraUpdated = true
+    end
+  end
 end
 
 -- Return the profile to ACR, so it can be read.
