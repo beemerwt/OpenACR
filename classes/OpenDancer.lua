b2a9ac00e42c649589ee860e4286b721872bd235
beemerwt <beemerwt@gmail.com>
2022-09-10 14:43:33 -0500

    Added dancer

---
+++ classes/OpenDancer.lua
@@ -0,0 +1,27 @@
+local profile = {
+
+}
+
+local Buffs = {
+
+}
+
+local Skills = {
+
+}
+
+local function GetBestDancePartner()
+  -- Ninja → Reaper/Monk → Dragoon/Samurai → Black Mage/Red Mage → Summoner → Machinist → Bard → Dancer
+end
+
+function profile.Cast()
+end
+
+function profile.Draw()
+end
+
+function profile.OnLoad()
+
+end
+
+return profile
