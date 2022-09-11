8a51f4fe851d85c4340cb8b08a7b84c79963fc49
beemerwt <beemerwt@gmail.com>
2022-09-11 07:18:22 -0500

    Added basis for dragoon

--- classes/OpenDragoon.lua
+++ classes/OpenDragoon.lua
@@ -1,14 +1,47 @@
-local Dragoon = {}
+local Dragoon = {
 
-function Dragoon.Cast()
+  phase = ""
+}
+
+local Skills = {
+  TrueThrust = -1,
+  VorpalThrust = -1, -- Combos with TrueThrust
+
+  LifeSurge = -1, -- First weapon skill afterwards is crit, increases damage when already under crit bonus, absorbs portion of damage as HP, can't be applied to DOT
+
+  PiercingTalon = -1, -- Ranged
+
+  Disembowel = -1,
+  FullThrust = -1,
+  LanceCharge = -1,
+  ChaosThrust = -1,
+}
+
+function Dragoon:Cast(target)
+  -- Lancer
+  if self.phase == "DEFENSIVE" then
+    
+  end
+
+  if self.phase == "ATTACK" then
+    
+  end
+
+  if IsReady(target.id, Skills.PiercingTalon) then
+    return next(target.id, Skills.PiercingTalon)
+  end
+end
+
+function Dragoon:Draw()
 
 end
 
-function Dragoon.Draw()
+function Dragoon:OnUpdate()
 
+  self.phase = "DAMAGE"
 end
 
-function Dragoon.OnLoad()
+function Dragoon:OnLoad()
 
 end
 
