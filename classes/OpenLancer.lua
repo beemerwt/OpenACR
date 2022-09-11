c74705131a94e2b3624d3b20c213dd5e5170a343
beemerwt <beemerwt@gmail.com>
2022-09-11 09:24:27 -0500

    Finalize lancer

--- classes/OpenLancer.lua
+++ classes/OpenLancer.lua
@@ -22,16 +22,22 @@ function Lancer:Cast(target)
   local playerHasPowerSurge = HasBuff(Player.id, Buffs.PowerSurge, 0, 3)
   local playerHasLifeSurge = HasBuff(Player.id, Buffs.LifeSurge)
 
+  local isFullThrustCapable = IsCapable(Skills.FullThrust)
+
   -- Just cast on cooldown
   if ReadyCast(Player.id, Skills.LanceCharge) then return true end
 
   if Player.lastcomboid == Skills.VorpalThrust then
     -- Cast life surge before Full Thrust for max damage
-    if not playerHasLifeSurge then
+    if not playerHasLifeSurge and isFullThrustCapable then
       if ReadyCast(Player.id, Skills.LifeSurge) then return true end
     end
 
     if ReadyCast(target.id, Skills.FullThrust) then return true end
+  end
+
+  if Player.lastcomboid == Skills.Disembowel then
+    if ReadyCast(target.id, Skills.ChaosThrust) then return true end
   end
 
   if Player.lastcomboid == Skills.TrueThrust then
@@ -39,16 +45,12 @@ function Lancer:Cast(target)
       if ReadyCast(target.id, Skills.Disembowel) then return true end
     end
 
-    if ReadyCast(target.id, Skills.VorpalThrust) then return true end
-  end
-
-  if Player.lastcomboid == Skills.Disembowel then
-    -- Cast life surge before Chaos Thrust for max damage
-    if not playerHasLifeSurge then
+    -- Before you can cast Full Thrust, opimize so that it can do the most damage
+    if not playerHasLifeSurge and not isFullThrustCapable then
       if ReadyCast(Player.id, Skills.LifeSurge) then return true end
     end
 
-    if ReadyCast(target.id, Skills.ChaosThrust) then return true end
+    if ReadyCast(target.id, Skills.VorpalThrust) then return true end
   end
 
   if ReadyCast(target.id, Skills.TrueThrust) then return true end
