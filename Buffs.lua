655bc536c1531ce0afdd07664b01081b92244f1f
beemerwt <beemerwt@gmail.com>
2022-09-16 07:54:14 -0500

    Sorted buffs and skills by alphabetical order

--- Buffs.lua
+++ Buffs.lua
@@ -1,60 +1,39 @@
 Buffs = {
-  -- Gladiator  
-  IronWill = 79,
-
-  -- Marauder
-  SurgingTempest = -1,
-  Defiance = 91,
-
-  -- Pugilist
-  RaptorForm = 108,
-  CoeurlForm = 109,
-  OpoOpoForm = 107,
-  DisciplinedFist = 3001,
-
-  -- Lancer
-  PowerSurge = 2720, -- Damage increase
-  LifeSurge = 116,
-
-  -- Monk
-
-  -- Ninja
-  Mudra       = 496,
-  TrickAttack = 3254,
-  Mug         = 638,
-  RaijuReady  = 2690,
-  Doton       = 501,
-  Kassatsu    = 497,
-  Suiton      = 507,
-  TenChiJin   = 1186,
-  KamaitachiReady = 2723,
-
-  -- Samurai
-  Fuka = 1299, -- Cooldown Reduction
-  Fugetsu = 1298, -- Increase Damage
-  Higanbana = 1228, -- Damage over Time
-
-  -- Reaper
-  SoulReaver = 2587,
-  DeathsDesign = 2586,
-
-  EnhancedGibbet = 2588,
-  EnhancedGallows = 2589,
-
-  ImmortalSacrifice = -1,
-  SoulSow = -1,
-
-  -- Dancer
-  ClosedPosition = 1823,
-  Peloton = 1199,
-  SilkenSymmetry = 2693, -- Reverse Cascade
-
-  -- Blue Mage
-  ToadOil = 1737,
-  BasicInstinct = 2498,
-  Swiftcast = 167,
-  Bristle = 1716, -- It's called boost
-  BadBreath = 18,
-  PeculiarLight = 1721,
-  Offguard = 1717,
+	BadBreath = 18,
+	BasicInstinct = 2498,
+	Bristle = 1716,
+	ClosedPosition = 1823,
+	CoeurlForm = 109,
+	DeathsDesign = 2586,
+	Defiance = 91,
+	DisciplinedFist = 3001,
+	Doton = 501,
+	EnhancedGallows = 2589,
+	EnhancedGibbet = 2588,
+	Fugetsu = 1298,
+	Fuka = 1299,
+	Higanbana = 1228,
+	ImmortalSacrifice = -1,
+	IronWill = 79,
+	KamaitachiReady = 2723,
+	Kassatsu = 497,
+	LifeSurge = 116,
+	Mudra = 496,
+	Mug = 638,
+	Offguard = 1717,
+	OpoOpoForm = 107,
+	PeculiarLight = 1721,
+	Peloton = 1199,
+	PowerSurge = 2720,
+	RaijuReady = 2690,
+	RaptorForm = 108,
+	SilkenSymmetry = 2693,
+	SoulReaver = 2587,
+	SoulSow = -1,
+	Suiton = 507,
+	SurgingTempest = -1,
+	Swiftcast = 167,
+	TenChiJin = 1186,
+	ToadOil = 1737,
+	TrickAttack = 3254,
 }
